299604c765d5edb2be75b7461c806c7f704d2a17
Harun Akgul <46harunakgul@gmail.com>
2022-09-03 17:21:46 +0300

    Adding flash animate and sound js file.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-alert("Hello")
+let buttonColours = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
+
+function nextSequence(){
+    let randomnumber = Math.floor(Math.random() * 4);
+    let randomChosenColour = buttonColours[randomnumber];
+
+    gamePattern.push(randomChosenColour);   
+    
+}
+
+
+$("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+let audio = new Audio("sounds" + randomChosenColour + ".mp3");
+audio.play();
