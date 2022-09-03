56332e396db903431cc3e962434cae58a8e085b5
Harun Akgul <46harunakgul@gmail.com>
2022-09-03 17:53:09 +0300

    Adding Buttons Click and Sound js file.

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 let buttonColours = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
+let userClickedPattern = [];
+
+$(".btn").click(function(){
+    let userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    console.log(userClickedPattern)
+});
+
+
 
 function nextSequence(){
     let randomnumber = Math.floor(Math.random() * 4);
     let randomChosenColour = buttonColours[randomnumber];
 
     gamePattern.push(randomChosenColour);   
-    
+
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    playSound(randomChosenColour);
+}
+
+function playSound(name){
+    let audio = new Audio("sounds" + name + ".mp3");
+    audio.play();
 }
 
 
-$("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
-let audio = new Audio("sounds" + randomChosenColour + ".mp3");
-audio.play();
+
+
